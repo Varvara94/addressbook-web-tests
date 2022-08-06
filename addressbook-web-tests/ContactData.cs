73d60d07e1a736ac5d988a12bbdc6e45ba521d05
Varvara94 <VarvaraKozlova94@yandex.ru>
2022-08-06 10:57:29 +0700

    Задание №4 Создаие контакта

---
+++ addressbook-web-tests/ContactData.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebAddressbookTests
+{
+    internal class ContactData
+    {
+        private string firstname;
+        private string lastname;
+        //private string email = "";
+
+        public ContactData(string firstname, string lastname)
+        {
+            this.firstname = firstname;
+            this.lastname = lastname;
+            
+        }
+        public string Firstname
+        { get { return Firstname; }
+    set { Firstname = value; } }
+
+       
+        public string Lastname
+        {
+            get { return Lastname; }
+            set { Lastname = value; }
+        }
+
+       // public string Email
+       // {
+           // get { return email; }
+           // set { email = value; }
+        }
+    }
+
+
